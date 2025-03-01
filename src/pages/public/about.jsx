ad7d362fb787a3a425b7c923db78bfc722b58d18
hachisnail <jeffersontalagtag06@gmail.com>
2025-03-02 00:37:01 +0800

    header

--- src/pages/public/about.jsx
+++ src/pages/public/about.jsx
@@ -9,10 +9,10 @@ const about = () => {
   return (
     <>
       <div className="w-auto z mx-auto pt-36 min-h-screen bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: "url('./assets/06-AfternoonMealOfTheWorker 1.png')" }}>
+      style={{ backgroundImage: "url('./assets/06-AfternoonMealOfTheWorker 1.png')" } }>
         <div className='mx-5 w-auto'>
           <h1>ABOUT</h1>
-      </div>
+        </div>
       </div>
     </>
   );
