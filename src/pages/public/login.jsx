8faec6ab85fbcdf3eb2ca68ae7dcd11cc661e778
hachisnail <jeffersontalagtag06@gmail.com>
2025-03-02 01:51:55 +0800

    header and background revamped

--- src/pages/public/login.jsx
+++ src/pages/public/login.jsx
@@ -7,9 +7,49 @@ const login = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div className='max-w-7xl mx-auto'>
-        <h1>LOGIN</h1>
-    </div>
+    <>
+      <div className="w-auto z mx-auto pt-7 min-h-screen bg-cover bg-center bg-no-repeat overflow-hidden"
+      style={{ backgroundImage: "url('./assets/06-AfternoonMealOfTheWorker 1.png')" }}>
+      <nav className=" w-[100vw] bg-transparent">
+        <div className='flex mx-20 h-28 bg-transparent  border-b-2 border-white justify-between'>
+          <div className='flex'> 
+
+            <img src="./assets/LOGO.png" alt="" className='max-h-20 my-auto'/>
+
+            <div className='h-20 w-1 my-auto rounded-2xl bg-white ml-5'>
+            </div>
+
+            <Link to="/" className='my-auto leading-6 text-3xl font-bold ml-2 text-white'> 
+                <span>Museo <br />Bulawan</span>
+            </Link>
+          </div>
+
+          <div className='my-auto text-2xl font-semibold shadow-black w-auto text-white'>
+            <NavLink to="/" className='mx-2'>
+              Home
+            </NavLink>
+            
+            <NavLink to="/news&events" className='mx-2'>
+              News & Events
+            </NavLink>
+
+            <NavLink to="/about" className='mx-2'>
+              About
+            </NavLink>
+          </div>
+
+        </div>
+      </nav>
+      <div className='mx-5 w-auto flex '>
+         
+        </div>
+          
+      </div>
+      <div className='w-auto z mx-auto pt-36 min-h-screen bg-cover bg-center bg-no-repeat'
+       style={{ backgroundImage: "url('./assets/06-AfternoonMealOfTheWorker 1.png')" }}>
+
+      </div>
+    </>
   );
 };
 
--- src/pages/public/newsandevents.jsx
+++ src/pages/public/newsandevents.jsx
@@ -2,15 +2,55 @@ import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 
-const NewsAndEvents = () => {
+const newsandevents = () => {
   const { isAuthenticated } = useAuth();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div className='max-w-7xl mx-auto'>
-        <h1>News And Events</h1>
-    </div>
+    <>
+      <div className="w-auto z mx-auto pt-7 min-h-80 bg-white overflow-hidden">
+      <nav className=" w-[100vw] bg-transparent">
+        <div className='flex mx-20 h-28 bg-transparent  border-b-2 border-black justify-between'>
+          <div className='flex'> 
+
+            <img src="./assets/LOGO.png" alt="" className='max-h-20 my-auto'/>
+
+            <div className='h-20 w-1 my-auto rounded-2xl bg-black ml-5'>
+            </div>
+
+            <Link to="/" className='my-auto leading-6 text-3xl font-bold ml-2 text-black'> 
+                <span>Museo <br />Bulawan</span>
+            </Link>
+          </div>
+
+          <div className='my-auto text-2xl font-semibold shadow-black w-auto text-black'>
+            <NavLink to="/" className='mx-2'>
+              Home
+            </NavLink>
+            
+            <NavLink to="/news&events" className='mx-2'>
+              News & Events
+            </NavLink>
+
+            <NavLink to="/about" className='mx-2'>
+              About
+            </NavLink>
+          </div>
+
+        </div>
+      </nav>
+      <div className='h-100 mt-3 flex  bg-cover bg-center bg-no-repeat' 
+      style={{ backgroundImage: "url('./assets/DSC_0099.png')" }}>
+         
+        </div>
+          
+      </div>
+
+      <div className='w-auto  mx-auto pt-36 min-h-screen bg-white'>
+
+      </div>
+    </>
   );
 };
 
-export default NewsAndEvents;
+export default newsandevents;
