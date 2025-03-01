2a25659c3c822485dea3c34b053beb3daa131c19
hachisnail <jeffersontalagtag06@gmail.com>
2025-03-02 01:20:18 +0800

    header revamp

--- src/components/headers/LandingHeader.jsx
+++ src/components/headers/LandingHeader.jsx
@@ -8,46 +8,40 @@ const LandingHeader = () => {
 
   return (
     
-    <header className="bg-transparent shadow-sm fixed z-50">
+    <header className="bg-transparent w-full shadow-sm fixed z-50">
       <div className='flex w-auto h-7 bg-[#1C1B19] justify-between px-5'>
         <span className=' text-white text-xs h-fit w-fit my-auto'>Open Daily 9:00am-5:00pm, Monday- Friday, Close During Holidays</span>
-        <NavLink to="/login" className='mx-2'>
-        <span className='text-white text-xs my-auto cursor-pointer'>
-          Login
-        </span>
-        </NavLink>
-      </div>
-
-      <nav className=" w-[100vw] bg-transparent">
-        <div className='flex mx-20 h-28 bg-transparent  border-b-2 border-white justify-between'>
-          <div className='flex'> 
-
-            <img src="./assets/LOGO.png" alt="" className='max-h-20 my-auto'/>
-
-            <div className='h-20 w-1 my-auto rounded-2xl bg-white ml-5'>
-            </div>
-
-            <Link to="/" className='my-auto leading-6 text-3xl font-bold ml-2 text-white'> 
-                <span>Museo <br />Bulawan</span>
-            </Link>
-          </div>
-
-          <div className='my-auto text-2xl font-semibold shadow-black w-auto text-white'>
+        <div className='w-auto'>
             <NavLink to="/" className='mx-2'>
-              Home
+              <span className='text-white text-xs my-auto cursor-pointer'>
+                Home
+              </span>
+            </NavLink>
+
+            <NavLink to="/news&events" className='mx-2'>
+            <span className='text-white text-xs my-auto cursor-pointer'>
+              News & Events
+            </span>
             </NavLink>
             
-            <NavLink to="/news&events" className='mx-2'>
-              News & Events
-            </NavLink>
-
             <NavLink to="/about" className='mx-2'>
+            <span className='text-white text-xs my-auto cursor-pointer'>
               About
+            </span>
             </NavLink>
-          </div>
+        
+        
+            <NavLink to="/login" className='mx-2'>
+              <span className='text-white text-xs my-auto cursor-pointer'>
+                Login
+              </span>
+            </NavLink>
 
-        </div>
-      </nav>
+            
+          </div>
+      </div>
+
+      
     </header>
   );
 };
